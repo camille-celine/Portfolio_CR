a72238250793e22d445f6d9f9151864ff9e9c22b
Camille Rey <camille.celine.rey@gmail.com>
2024-07-10 14:43:19 +0200

    layout

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../style/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="name">Camille Rey</div>
+      <button>Download CV</button>
+    </div>
+  )
+}
+
+export default Navbar; 
+
